5cad4a4b365049958e26f2ffbbe87fcf03ec8418
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-04-24 20:55:55 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,4 +1,4 @@
-const elementsByTagName = document.querySelectorAll("p");
+const elementsByTagName = document.querySelector("p");
 
 const ulCategories = document.createElement('ul');
 ulCategories.id = 'categories';
@@ -79,10 +79,6 @@ liCategoriesTechnologies.appendChild(ulItemsTechnologies);
 ulCategories.appendChild(liCategoriesTechnologies);
 
 document.body.append(elementsByTagName, ulCategories , );
-
-
-
-
 const categoriesItem = document.querySelectorAll(".item");
 console.log(`В списке ${categoriesItem.length} категорий`);
 categoriesItem.forEach((item) => {
@@ -92,54 +88,3 @@ categoriesItem.forEach((item) => {
     `Категория: ${categoriesHeader} \nКоличество элементов: ${subCategoriesItem}`
   );
 });
-// В HTML есть список категорий `ul#categories`.
-
-// ```html
-/* <ul id="categories">
-  <li class="item">
-    <h2>Животные</h2>
-
-    <ul>
-      <li>Кот</li>
-      <li>Хомяк</li>
-      <li>Лошадь</li>
-      <li>Попугай</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Продукты</h2>
-Products
-    <ul>
-      <li>Хлеб</li>
-      <li>Петрушка</li>
-      <li>Творог</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Технологии</h2>
-Technologies
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>React</li>
-      <li>Node</li>
-    </ul>
-  </li>
-</ul> */
-// ```
-
-// Напиши скрипт, который выполнит следующие операции.
-
-// Посчитает и выведет в консоль количество категорий в `ul#categories`, то есть
-// элементов `li.item`. Получится `'В списке 3 категории.'`.
-
-// Для каждого элемента `li.item` в списке `ul#categories`, найдет и выведет в
-// консоль текст заголовка элемента (тега h2) и количество элементов в категории
-// (всех вложенных в него элементов `li`).
-
-// Например для первой категории получится:
-
-// - Категория: Животные
-// - Количество элементов: 4
-
